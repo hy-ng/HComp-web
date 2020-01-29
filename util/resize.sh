b9fc253271eef254532a63056a5cd5bb08eecfc2
gabrielmaia7 <gabrielmaiarocha@gmail.com>
2020-01-29 18:04:10 +0000

    updated website

--- util/resize.sh
+++ util/resize.sh
@@ -3,6 +3,6 @@
 # usage: resize.sh original.png
 # output: original-150.jpg
 s=$1
-#s=${s##*/}
-s=${s%.*}
-convert $1 -thumbnail 150x150^ -gravity center -extent 150x150 $s-150.jpg
+s=${s%%.*}
+# Using just {s}-150.jpg was keeping a trailling whitespace on the file name
+convert $1 -thumbnail 150x150^ -gravity center -extent 150x150 "$(echo -e "${s}" | tr -d '[:space:]')"-150.jpg
