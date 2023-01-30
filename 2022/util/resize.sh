9dada13db1234631b522152b56baf66ed34a98c7
Carlos <carlos@38.240.21.198.tigernet.wifi.dyn.clemson.edu>
2023-01-30 14:25:24 -0500

    skip files for archiving

--- 2022/util/resize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Reshapes an image to 150x150
-# usage: resize.sh original.png
-# output: original-150.jpg
-s=$1
-s=${s%%.*}
-# Using just {s}-150.jpg was keeping a trailling whitespace on the file name
-convert $1 -thumbnail 150x150^ -gravity center -extent 150x150 "$(echo -e "${s}" | tr -d '[:space:]')"-150.jpg
